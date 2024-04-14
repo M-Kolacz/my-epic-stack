28c2cf133d4fc6dc1da404e39965784ae8ea7ce2
Michał Kołacz <michal.kolacz45@gmail.com>
2024-04-14 18:18:38 +0200

    Dev (#5)
    * feat: add button
    
    * Feat/add prisma (#3)
    
    * feat: add prisma client
    
    * fix: correct setup for dockerfile
    
    * feat: add persistet volume
    
    * feat: add new title in user route (#4)

---
+++ app/utils/db.server.ts
@@ -0,0 +1,13 @@
+import { remember } from "@epic-web/remember";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = remember("prisma", () => {
+  // NOTE: if you change anything in this function you'll need to restart
+  // the dev server to see your changes.
+
+  // Feel free to change this log threshold to something that makes sense for you
+
+  const client = new PrismaClient({});
+  client.$connect();
+  return client;
+});
