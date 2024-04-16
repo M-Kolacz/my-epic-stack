223425dab8aac852381f9f6dcdcff93117d3dae3
Michał Kołacz <michal.kolacz45@gmail.com>
2024-04-16 19:00:41 +0200

    feat: add svg favicon

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -5,6 +6,10 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+
+export const links: LinksFunction = () => {
+  return [{ rel: "icon", href: "/favicon.svg", type: "image/svg+xml" }];
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
