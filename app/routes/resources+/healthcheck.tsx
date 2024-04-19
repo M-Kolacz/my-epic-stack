7ce87897f49afc76c529672ab837db58a04a5f38
Michał Kołacz <michal.kolacz45@gmail.com>
2024-04-19 10:30:31 +0200

    feat: add healthcheck

--- app/routes/resources+/healthcheck.tsx
+++ app/routes/resources+/healthcheck.tsx
@@ -1,3 +1,25 @@
-export const loader = () => {
-  return new Response("OK");
+import { type LoaderFunctionArgs } from "@remix-run/node";
+import { prisma } from "../../utils/db.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const host =
+    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+
+  try {
+    // if we can connect to the database and make a simple query
+    // and make a HEAD request to ourselves, then we're good.
+    await Promise.all([
+      prisma.user.count(),
+      fetch(`${new URL(request.url).protocol}${host}`, {
+        method: "HEAD",
+        headers: { "X-Healthcheck": "true" },
+      }).then((response) => {
+        if (!response.ok) return Promise.reject(response);
+      }),
+    ]);
+    return new Response("OK");
+  } catch (error) {
+    console.log("Healthcheck ❌", { error });
+    throw new Response("ERROR", { status: 500 });
+  }
 };
