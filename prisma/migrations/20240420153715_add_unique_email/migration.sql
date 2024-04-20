86cbfc2a7cf8aee6a917124c9f17baab548b9d2e
Michał Kołacz <michal.kolacz45@gmail.com>
2024-04-20 19:05:56 +0200

    Feat/deploy migrations (#23)
    * Feat/public assets (#11)
    
    * feat: add svg favicon
    
    * feat: add tailwindcss
    
    * feat: add ts reset (#12)
    
    * Feat/flat routes (#13)
    
    * feat: add basic routes
    
    * feat: add resource route
    
    * feat: add healthcheck
    
    * feat: add paths to tsconfig (#14)
    
    * Feat/new routes (#15)
    
    * feat: add new routes
    
    * feat: add shadcn components
    
    * feat: add link component
    
    * Feat/login form (#16)
    
    * feat: add basic login form
    
    * feat: add Field component
    
    * feat: add honeypot protection
    
    * feat: add csrf protection
    
    * Refactor/better log (#17)
    
    * refactor: add better error logs
    
    * refactor: correct semantic
    
    * refactor: adjust csrf logs
    
    * Feat/env variables (#19)
    
    * feat: add support for env variables
    
    * refactor: add missing env variables
    
    * Feat/add users notes (#21)
    
    * fix: add sqlite.db to gitigonre
    
    * fix: delete database file
    
    * fix: add db to gitignore
    
    * feat: apply two migrations
    
    * feat: add shortcut to Dockerfile (#22)

---
+++ prisma/migrations/20240420153715_add_unique_email/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - Made the column `email` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("createdAt", "email", "id", "name", "updatedAt", "username") SELECT "createdAt", "email", "id", "name", "updatedAt", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
