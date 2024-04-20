4b115a75083cc1f0b876b537eb6d5892993545de
Michał Kołacz <michal.kolacz45@gmail.com>
2024-04-20 19:40:46 +0200

    Feat/deploy new migration (#25)
    * Feat/public assets (#11)
    
    * feat: add svg favicon
    
    * feat: add tailwindcss
    
    * feat: add ts reset (#12)
    
    * Feat/flat routes (#13)
    
    * feat: add basic routes
    
    * feat: add resource route
    
    * feat: add healthcheck
    
    * feat: add paths to tsconfig (#14)
    
    * Feat/new routes (#15)
    
    * feat: add new routes
    
    * feat: add shadcn components
    
    * feat: add link component
    
    * Feat/login form (#16)
    
    * feat: add basic login form
    
    * feat: add Field component
    
    * feat: add honeypot protection
    
    * feat: add csrf protection
    
    * Refactor/better log (#17)
    
    * refactor: add better error logs
    
    * refactor: correct semantic
    
    * refactor: adjust csrf logs
    
    * Feat/env variables (#19)
    
    * feat: add support for env variables
    
    * refactor: add missing env variables
    
    * Feat/add users notes (#21)
    
    * fix: add sqlite.db to gitigonre
    
    * fix: delete database file
    
    * fix: add db to gitignore
    
    * feat: apply two migrations
    
    * feat: add shortcut to Dockerfile (#22)
    
    * test: add new migration (#24)

---
+++ prisma/migrations/20240420173247_remove_email/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "username" TEXT NOT NULL,
+    "name" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+INSERT INTO "new_User" ("createdAt", "id", "name", "updatedAt", "username") SELECT "createdAt", "id", "name", "updatedAt", "username" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
