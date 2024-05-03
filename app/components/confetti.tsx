f10b2b462c537d9ed900b65d534dab830746ab87
Michał Kołacz <michal.kolacz45@gmail.com>
2024-05-03 08:40:11 +0200

    feat: add Confetti (#31)

---
+++ app/components/confetti.tsx
@@ -0,0 +1,21 @@
+import ReactConfetti from "react-confetti";
+import { ClientOnly } from "remix-utils/client-only";
+
+export const Confetti = ({ id }: { id?: string | null }) => {
+  if (!id) return null;
+
+  return (
+    <ClientOnly>
+      {() => (
+        <ReactConfetti
+          key={id}
+          run={!!id}
+          recycle={false}
+          numberOfPieces={1000}
+          width={window.innerWidth}
+          height={window.innerHeight}
+        />
+      )}
+    </ClientOnly>
+  );
+};
