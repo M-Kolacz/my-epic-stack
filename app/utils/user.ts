723c57ef038bb9c519576edaa49c6cfa156b6327
Michał Kołacz <michal.kolacz45@gmail.com>
2024-05-14 11:38:03 +0200

    feat: add permissions and roles utils

--- app/utils/user.ts
+++ app/utils/user.ts
@@ -18,3 +18,56 @@ export const useUser = () => {
 
   return maybeUser;
 };
+
+export const action = ["create", "read", "update", "delete"] as const;
+export const entity = ["user", "note"] as const;
+export const access = ["own", "any", "own,any", "any,own"] as const;
+export const role = ["admin", "user"] as const;
+
+export type Action = (typeof action)[number];
+export type Entity = (typeof entity)[number];
+export type Access = (typeof access)[number];
+export type Role = (typeof role)[number];
+export type PermissionString =
+  | `${Action}:${Entity}`
+  | `${Action}:${Entity}:${Access}`;
+
+export const parsePermissionString = (permissionString: PermissionString) => {
+  const [action, entity, access] = permissionString.split(":") as [
+    Action,
+    Entity,
+    Access | undefined,
+  ];
+
+  return {
+    action,
+    entity,
+    access: access
+      ? (access.split(",") as Array<Extract<Access, "own" | "any">>)
+      : undefined,
+  };
+};
+
+export const userHasPermission = (
+  user: Pick<ReturnType<typeof useUser>, "roles"> | null,
+  permission: PermissionString
+) => {
+  if (!user) return false;
+  const { access, action, entity } = parsePermissionString(permission);
+  return user.roles.some((role) =>
+    role.permissions.some(
+      (permission) =>
+        action === permission.action &&
+        entity === permission.entity &&
+        (!access || access.includes(permission.access))
+    )
+  );
+};
+
+export const userHasRole = (
+  user: Pick<ReturnType<typeof useUser>, "roles"> | null,
+  role: Role
+) => {
+  if (!user) return false;
+  return user.roles.some((userRole) => userRole.name === role);
+};
